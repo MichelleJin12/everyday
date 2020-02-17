384fc0c76fcebd6fbdbcfe46f33385c8f94de05c
MichelleJin12 <53206331+MichelleJin12@users.noreply.github.com>
2020-02-17 16:51:37 +0900

    Create 2606_1.cpp

---
+++ 2606_1.cpp
@@ -0,0 +1,49 @@
+//처음 만들어 본 깃헙.....
+
+//백준 바이러스
+//dfs로 풀었음
+
+#include <iostream>
+#include <vector>
+
+#define COM_MAX 101
+int check[COM_MAX] = { 0 };
+std::vector<int> v[COM_MAX];
+void dfs(int x);
+
+int main()
+{
+	int c; int cnt = 0;
+	std::cin >> c;
+	int e;
+	std::cin >> e;
+	while (e--)
+	{
+		int a, b;
+		std::cin >> a >> b;
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+	dfs(1);
+
+	for (int i = 1; i <= c; i++)
+	{
+		if (check[i] == 1)
+			cnt++;
+	}
+	std::cout << cnt-1;
+
+	return 0;
+}
+
+void dfs(int x)
+{
+	check[x] = 1;
+	for (int i = 0; i < v[x].size(); i++)
+	{
+		if (check[v[x][i]] == 0)
+		{
+			dfs(v[x][i]);
+		}
+	}
+}
